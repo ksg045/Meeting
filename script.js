c5ffa8c3f8126af5b7c947511e7199cca0a45943
ksg045 <127873528+ksg045@users.noreply.github.com>
2023-03-14 21:04:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".btn");
+ let position;
+
+btn.addEventListener("mouseover", 
+function () {
+    position? (position = 0): (position
+        = 150);
+
+btn.style.transform = `translate(${
+
+position}px, 0px)`;
+
+btn.style.transition= "all 0.3s ease";
+
+});
+
+const button = document.getElementById('button');
+button.addEventListener('click', () => alert("oh When we meet there is one rule , no matter what happens  you are not allowed to fall in love with me!! , I think that won't be a problem , dm me for this"));
